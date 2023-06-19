2cfa79b189ad0716a30be8aab971d0eea7fa402e
Moussa <salisoumoussa1@gmail.com>
2023-06-19 14:54:31 +0200

    Aggiungere i file di progetto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MauiVerter.MVVM.Views;
+
+namespace MauiVerter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new MenuView());
+		//MainPage = new ConverterView();
+	}
+}
---
+++ MVVM/ViewModels/ConverterViewModel.cs
@@ -0,0 +1,58 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UnitsNet;
+
+namespace MauiVerter.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ConverterViewModel
+    {
+        public string QuantityName { get; set; }
+        public ObservableCollection<string> FromMeasures { get; set; }
+        public ObservableCollection<string> ToMeasures { get; set; }
+        public string CurrentFromMeasures { get; set; }
+        public string CurrentToMeasures { get; set; }
+        public double FormValue { get; set; } = 1;
+        public double ToValue { get; set; }
+        public ICommand ReturnCommand =>
+            new Command(() =>
+            {
+                Convert();
+            });
+
+        public ConverterViewModel(string quantityName) 
+        {
+            QuantityName = quantityName;
+            FromMeasures = LoadMeasure();
+            ToMeasures = LoadMeasure();
+            CurrentFromMeasures = FromMeasures.FirstOrDefault();
+            CurrentToMeasures = ToMeasures.Skip(1).FirstOrDefault();
+            Convert();
+        }
+
+        public void Convert()
+        {
+            var result = UnitConverter.ConvertByName(FormValue, QuantityName, CurrentFromMeasures, CurrentToMeasures);
+
+            ToValue = result;
+        }
+
+        private ObservableCollection<string> LoadMeasure()
+        {
+            var types = 
+                Quantity.Infos
+                .FirstOrDefault(x => x.Name == QuantityName)
+                .UnitInfos
+                .Select(u => u.Name)
+                .ToList();
+
+            return new ObservableCollection<string>(types);
+        }
+    }
+}
